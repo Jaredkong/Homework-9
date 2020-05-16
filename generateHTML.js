88a345aa1e4ada002cfc3cc458dc2f9dc8173905
Jared Kong <jlkong001@yahoo.com>
2020-05-16 03:13:27 -0700

    pdf generates

---
+++ generateHTML.js
@@ -0,0 +1,233 @@
+'use strict';
+
+const colors = {
+  green: {
+    wrapperBackground: '#E6E1C3',
+    headerBackground: '#C1C72C',
+    headerColor: 'black',
+    photoBorderColor: '#black'
+  },
+  blue: {
+    wrapperBackground: '#5F64D3',
+    headerBackground: '#26175A',
+    headerColor: 'white',
+    photoBorderColor: '#73448C'
+  },
+  pink: {
+    wrapperBackground: '#879CDF',
+    headerBackground: '#FF8374',
+    headerColor: 'white',
+    photoBorderColor: '#FEE24C'
+  },
+  red: {
+    wrapperBackground: '#DE9967',
+    headerBackground: '#870603',
+    headerColor: 'white',
+    photoBorderColor: 'white'
+  }
+};
+
+function generateHTML(data) {
+    console.log("this is data" + JSON.stringify(data))
+    console.log("this is favecolor" + JSON.stringify(data.faveColor))
+  return `<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+      <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+      <title>Document</title>
+      <style>
+          @page {
+            margin: 0;
+          }
+         *,
+         *::after,
+         *::before {
+         box-sizing: border-box;
+         }
+         html, body {
+         padding: 0;
+         margin: 0;
+         }
+         html, body, .wrapper {
+         height: 100%;
+         }
+         .wrapper {
+         background-color: ${colors[data.faveColor].wrapperBackground};
+         
+         padding-top: 100px;
+         }
+         body {
+         background-color: white;
+         -webkit-print-color-adjust: exact !important;
+         font-family: 'Cabin', sans-serif;
+         }
+         main {
+         background-color: #E9EDEE;
+         height: auto;
+         padding-top: 30px;
+         }
+         h1, h2, h3, h4, h5, h6 {
+         font-family: 'BioRhyme', serif;
+         margin: 0;
+         }
+         h1 {
+         font-size: 3em;
+         }
+         h2 {
+         font-size: 2.5em;
+         }
+         h3 {
+         font-size: 2em;
+         }
+         h4 {
+         font-size: 1.5em;
+         }
+         h5 {
+         font-size: 1.3em;
+         }
+         h6 {
+         font-size: 1.2em;
+         }
+         .photo-header {
+         position: relative;
+         margin: 0 auto;
+         margin-bottom: -50px;
+         display: flex;
+         justify-content: center;
+         flex-wrap: wrap;
+         background-color: ${colors[data.faveColor].headerBackground};
+         color: ${colors[data.faveColor].headerColor};
+         padding: 10px;
+         width: 95%;
+         border-radius: 6px;
+         }
+         .photo-header img {
+         width: 250px;
+         height: 250px;
+         border-radius: 50%;
+         object-fit: cover;
+         margin-top: -75px;
+         border: 6px solid ${colors[data.faveColor].photoBorderColor};
+         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+         }
+         .photo-header h1, .photo-header h2 {
+         width: 100%;
+         text-align: center;
+         }
+         .photo-header h1 {
+         margin-top: 10px;
+         }
+         .links-nav {
+         width: 100%;
+         text-align: center;
+         padding: 20px 0;
+         font-size: 1.1em;
+         }
+         .nav-link {
+         display: inline-block;
+         margin: 5px 10px;
+         }
+         .workExp-date {
+         font-style: italic;
+         font-size: .7em;
+         text-align: right;
+         margin-top: 10px;
+         }
+         .container {
+         padding: 50px;
+         padding-left: 100px;
+         padding-right: 100px;
+         }
+
+         .row {
+           display: flex;
+           flex-wrap: wrap;
+           justify-content: space-between;
+           margin-top: 20px;
+           margin-bottom: 20px;
+         }
+
+         .card {
+           padding: 20px;
+           border-radius: 6px;
+           background-color: ${colors[data.faveColor].headerBackground};
+           color: ${colors[data.faveColor].headerColor};
+           margin: 20px;
+         }
+         
+         .col {
+         flex: 1;
+         text-align: center;
+         }
+
+         a, a:hover {
+         text-decoration: none;
+         color: inherit;
+         font-weight: bold;
+         }
+
+         @media print { 
+          body { 
+            zoom: .75; 
+          } 
+         }
+      </style>
+    </head>
+    <body>
+      <div class="wrapper">
+         <div class="photo-header">
+            <img src="${data.githubPicture}" alt="Photo of ${data.githubUsername}" />
+            <h1>Hi!</h1>
+            <h2>
+            My name is ${data.realName}!</h1>
+            <nav class="links-nav">
+               ${
+                 data.githubLocation
+                   ? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/${data.githubLocation}"><i class="fas fa-location-arrow"></i> ${data.githubLocation}</a>`
+                   : ''
+               }
+               <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
+                 data.githubhtmlUrl
+               }"><i class="fab fa-github-alt"></i> GitHub</a>
+            </nav>
+         </div>
+         <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <h3>Public Repositories</h3>
+                            <h4>${data.githubPublicRepos}</h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <h3>Followers</h3>
+                            <h4>${data.githubFollowers}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                        <h3>GitHub Stars</h3>
+                        <h4>${data.totalStars}</h4>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <h3>Following</h3>
+                        <h4>${data.githubFollowing}</h4>
+                    </div>
+               </div>
+            </div>
+         </main>
+      </div>
+   </body>
+</html>`;
+}   
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const inquirer = require('inquirer');
-const axios = require('axios')
+const axios = require('axios');
+const htmlpdf = require ('html-pdf');
+const open = require('open');
+const generateHTML = require('./generateHTML');
+
+let githubRepos, githubLocation, githubFollowers, githubFollowing, githubPublicRepos, githubhtmlUrl, githubPicture, realName;
+
+
 const questions = 
 
 [
@@ -11,49 +18,103 @@ const questions =
 
     {
         type: "list",
-        name: "color",
-        message: "What is your favorite color?",
-        choices: ["red","green","blue"]
+        name: "faveColor",
+        message: "What's your favorite color?",
+        choices: ["green", "blue", "pink", "red"]
 
     }
 ];
 
 
 
-function getUser (username) {
-        return axios
-          .get(
-            `https://api.github.com/users/${username}`
-          )
-          .then(
-              response => {console.log(response)
-                console.log(response.data.followers)
+async function getUser (username) {
+    try{
+        const response = await axios.get(`https://api.github.com/users/${username}`)
 
-               let githubFollowers = response.data.followers;
-               let githubLocation = response.data.location;
-               let githubPublicRepos = response.data.public_repos;
-               let githubFollowing = response.data.following;
-               let githubhtmlUrl = response.data.html_Url;
-            
-            
-            }  
-          )
+        githubPicture = response.data.avatar_url;
+        githubFollowers = response.data.followers;
+        githubLocation = response.data.location;
+        githubPublicRepos = response.data.public_repos;
+        githubFollowing = response.data.following;
+        githubhtmlUrl = response.data.html_url;
+        githubRepos = response.data.repos_url;
+        realName = response.data.name;
+                
+             
           
+    }
+    catch (err){
+        console.error(err)
+    }  
 
 
 };
+async function getTotalStars(username) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${username}/repos`)
+
+        console.log("hello" + response.data[1].stargazers_count)
+        console.log(parseInt(response.data[1].stargazers_count) + parseInt(response.data[5].stargazers_count) )
+
+        let totalStars = 0
+        for (var i=0; i < response.data.length; i++) {
+           
+            console.log(i)
+            totalStars += parseInt(response.data[i].stargazers_count)
+         
+        }
+        console.log("we adding numbers" + response.data[7].stargazers_count,response.data[5].stargazers_count,response.data[1].stargazers_count)
+        console.log("THIS IS WHATEVER" + totalStars)
+        return totalStars
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+
+
 
 async function init () {
-    const {github: githubUsername, faveColor } = await inquirer.prompt(questions);
-    console.log (`searching for ${githubUsername}`);
-    getUser(githubUsername);
-    
-        
+    try{
+        const {github: githubUsername, faveColor } = await inquirer.prompt(questions);
+        console.log (`searching for ${githubUsername}`);
+        getUser(githubUsername);
+        const totalStars = await getTotalStars(githubUsername);
+        console.log("here is your favorite color!!" + faveColor)
+        console.log("here r ur totl stars" + totalStars);
+        const html = generateHTML ({
+            githubUsername,
+            faveColor,
+            githubRepos, 
+            githubLocation, 
+            githubFollowers, 
+            githubFollowing, 
+            githubPublicRepos, 
+            githubhtmlUrl,
+            totalStars,
+            githubPicture,
+            realName
+        });
+        generatePDF(html);
+    }catch (err){
+        console.error(err);
+    }
+
+
               
           
 };
 
-    
+function generatePDF(html){
+    const resumeFilePath = './resume.pdf';
+    htmlpdf.create(html, { format: 'Tabloid' }).toFile(resumeFilePath, (err, res) => {
+        if (err) {
+            return console.error(err);
+        }
+    console.log('successfully created resume', res.filename);
+    open(resumeFilePath);
+  });
+}
 
 
 
